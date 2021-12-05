ebc84391602f3c77a0e2d0d0f2af45c692189d5b
AnuragKumar <anuragkumarrr@gmail.com>
2021-12-04 19:30:30 -0600

    second commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || '2000';
+const port = process.env.PORT || '2001';
 
 // React build output folder
 app.use(express.static(path.join(__dirname, 'build')))
